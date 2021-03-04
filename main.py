0ef803c1b721db9fc668e6170727926970f7fe3c
Chan Guruge <ygcjayalal@gmail.com>
2021-03-04 20:58:58 +0000

    Random Password genarator

--- main.py
+++ main.py
@@ -1,31 +1,76 @@
-heights = input("Enter students height :\n").split(" ")
+# marks = input("Enter all the Marks of the students :\n").split(" ")
+# for mark in range(0,len(marks)):
+#   marks[mark] = int(marks[mark])
 
-for x in range(0,len(heights)):
-  heights[x] = int(heights[x])
+# print("Students marks ",marks)
+
+# highest_score = 0
+# for mark in marks:
+#   if mark > highest_score:
+#     highest_score = mark
+
+# print(highest_score)
+import random
+
+a =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 
-height_list = []
-for height in heights:
-  height_list.append(int(height))
+b = ['1','2','3','4','5','6','7','8','9','0']
+
+c = ['!','@','#','$','%','^','&','*','(',')']
+
+letters = int(input("How many letters do you want to add :\n"))
+numbers = int(input("How many numbers do you want to add :\n"))
+symbols = int(input("How many symbols do you want to add :\n"))
+
+password_caractors = (letters+numbers+symbols)
+
+password = []
+
+for x in range(1,letters+1):
+  password.append(random.choice(a))
+
+for x in range(1,numbers+1):
+  password.append(random.choice(b))
+
+for x in range(1,symbols + 1):
+  password.append(random.choice(c))
+
+# Random Password generator
+
+random.shuffle(password)
+for x in password:
+  print(x,end="")
 
 
-print("Value List :",height_list)
-total_height = 0
-for max_val in height_list:
-  total_height += max_val
+# total_digits = (letters + numbers + symbols)
 
-print("total Height is : ",total_height)
+# password = []
+# for x in random.sample(a,letters):
+#   password.append(x)
+# for x in random.sample(b,numbers):
+#   password.append(x)
+# for x in random.sample(c,symbols):
+#   password.append(x)
 
-total_student = 0
-for student in range(0,len(height_list)):
-  total_student += 1
+# for p in random.sample(password,total_digits):
+#   print(p,end="")
 
-print("Total Students :",total_student)
 
-average = total_height/total_student
-print("Total Round Avg :",round(average))
-print("Total Avg :","%.2f" %average)
 
+
+
+
+
+
+
+
+
+
+
+
+
+  
 
 
 
