02f5c418c3a6b29742df537743298be958b6c3b2
Chan  Guruge <ygcjayalal@gmail.com>
2021-03-08 10:16:41 +0000

    Hang man Last

--- main.py
+++ main.py
@@ -1,121 +1,74 @@
 import random
+from hang_man_ascii import hangman,stages
+from replit import clear
 
-stages = ['''
-      _______
-     |/    |
-     |    (_)
-     |    \|/
-     |     |
-     |    / \
-     |
- jgs_|___''',
- '''   _______
-     |/      |
-     |      (_)
-     |      \|/
-     |       |
-     |      / 
-     |
- jgs_|___''','''
-       _______
-     |/      |
-     |      (_)
-     |      \|/
-     |       |
-
- jgs_|___''','''   
-      _______
-     |/      |
-     |      (_)
-     |      \|/
-     |
- jgs_|___''','''
-       _______
-     |/      |
-     |      (_)
-     |       |
-     |
- jgs_|___''','''
-      _______
-     |/      |
-     |      (_)
-     |
- jgs_|___''',''' 
-      _______
-     |/      |
-     |      
-
-     |
- jgs_|___''',]
-
-words = ["pencil","book","computer"]
-
-rand_word = random.choice(words).lower()
+print(hangman)
+word_list = ["book","computer","pencil","school"]
+chosen_word = random.choice(word_list)
 
 display = []
-len_word = len(rand_word)
-for word in range(len_word):
+for position in range(len(chosen_word)):
   display.append("_")
 
+last_disp = " ".join([str(eli) for eli in display])
+print("Random Word is :",last_disp)
 
+lives = 6
 
-print(display)
-print(rand_word)
-guess_time = 0
-success_word_count = 0
-
-
-counter = False
-while not counter:
-  guess_word = input("Enter a tetter : \n")
-  guess_time +=1
-  for letter in range(len_word):
-    position = rand_word[letter]
-    if position == guess_word:
-      display[letter] = position
-      success_word_count +=1
-  print(display)
-
-  # print("guess_time :",guess_time)
-  # print("success_word_count :",success_word_count)
-  stages_tracking = (success_word_count - guess_time)
-  # print("stages_tracking :",stages_tracking)
-
-  if not stages_tracking == -8:
-    current_stages = stages[stages_tracking]
-    print(current_stages)
-  else:
-    print("You Dead ")
-    counter = True
+end_of_game = False
+while not end_of_game:
+  guess = input("Guess a letter: ").lower()
+  clear()
+  for letter in range(len(chosen_word)):
+    position = chosen_word[letter]
+    if position == guess:
+      display[letter] = guess
+  last_disp = " ".join(str(eli)for eli in display)
+  print(last_disp)
   
 
+  if guess not in chosen_word:
+    print(stages[lives])
+    lives -=1
+    if lives == -1:
+      print("You Lost ")
+      end_of_game = True
+  
   if "_" not in display:
     print("You Won!")
-    counter = True
+    end_of_game = True
 
 
 
 
+# words = ["book","computer","pencil","school"]
+# random_word = random.choice(words)
+# print("Random Word is :",random_word)
+# random_word_length = range(len(random_word))
+# display = []
+# for text in random_word_length:
+#   display.append("_")
+# print(display)
 
-# counter = 0
-# while counter < len_word:
-#   guess_word = input("Enter a tetter : \n")
-#   for letter in range(len_word):
-#     position = rand_word[letter]
+# wrong_guess = 0
+# guess_again_and_again = False
+
+# while not guess_again_and_again:
+#   guess_word = input("Guess: ")
+#   for text in random_word_length:
+#     position = random_word[text]
 #     if position == guess_word:
-#       display[letter] = position
-#       counter +=1
+#       display[text] = guess_word
+
 #   print(display)
 
+#   if not "_" in display:
+#     print("You Won !")
+#     guess_again_and_again = True
 
-# print("You Won!")
-
-
-
-
-
-
-
-
+  
+#   if guess_word not  in display:
+#     wrong_guess += len(guess_word)
+#     print("I am Not in :", wrong_guess)
 
 
