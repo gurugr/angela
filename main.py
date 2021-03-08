21f403eb21623a170fc45f4318a2b20cf063f906
Chan  Guruge <ygcjayalal@gmail.com>
2021-03-08 18:11:10 +0000

    Prime Number Checker

--- main.py
+++ main.py
@@ -1,74 +1,24 @@
-import random
-from hang_man_ascii import hangman,stages
-from replit import clear
+n = int(input("Enter the number : "))
 
-print(hangman)
-word_list = ["book","computer","pencil","school"]
-chosen_word = random.choice(word_list)
+# def prime_checker(number):
+#   for num in range(2,number):
+#     if number % 2 ==0:
+#       print("no prime")
+#       break
+#   else:
+#     print("prime")
 
-display = []
-for position in range(len(chosen_word)):
-  display.append("_")
+def prime_checker(number):
+  is_prime = True
+  for x in range(2,number):
+    # 5 % 2 = 1
+    # 5 % 3 = 2
+    # 5 % 4 = 1
+    if number % x == 0:
+      is_prime = False
+  if not is_prime:
+    print("not prime")
+  else:
+    print("prime")
 
-last_disp = " ".join([str(eli) for eli in display])
-print("Random Word is :",last_disp)
-
-lives = 6
-
-end_of_game = False
-while not end_of_game:
-  guess = input("Guess a letter: ").lower()
-  clear()
-  for letter in range(len(chosen_word)):
-    position = chosen_word[letter]
-    if position == guess:
-      display[letter] = guess
-  last_disp = " ".join(str(eli)for eli in display)
-  print(last_disp)
-  
-
-  if guess not in chosen_word:
-    print(stages[lives])
-    lives -=1
-    if lives == -1:
-      print("You Lost ")
-      end_of_game = True
-  
-  if "_" not in display:
-    print("You Won!")
-    end_of_game = True
-
-
-
-
-# words = ["book","computer","pencil","school"]
-# random_word = random.choice(words)
-# print("Random Word is :",random_word)
-# random_word_length = range(len(random_word))
-# display = []
-# for text in random_word_length:
-#   display.append("_")
-# print(display)
-
-# wrong_guess = 0
-# guess_again_and_again = False
-
-# while not guess_again_and_again:
-#   guess_word = input("Guess: ")
-#   for text in random_word_length:
-#     position = random_word[text]
-#     if position == guess_word:
-#       display[text] = guess_word
-
-#   print(display)
-
-#   if not "_" in display:
-#     print("You Won !")
-#     guess_again_and_again = True
-
-  
-#   if guess_word not  in display:
-#     wrong_guess += len(guess_word)
-#     print("I am Not in :", wrong_guess)
-
-
+prime_checker(number=n)
